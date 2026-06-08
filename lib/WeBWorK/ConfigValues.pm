bc054477f1d722d35773f6efd242ee13f248331a
Kyle Claassen <claassen@rose-hulman.edu>
2026-06-08 04:26:13 +0000

    Fix typo

--- lib/WeBWorK/ConfigValues.pm
+++ lib/WeBWorK/ConfigValues.pm
@@ -933,7 +933,7 @@ sub getConfigValues ($ce) {
 			var  => 'LTI{v1p1}{enable_score_rounding}',
 			doc  => x('Enable score rounding'),
 			doc2 => x(
-				'If true, then score ratios are rounded to two decimal places before passing back the the LMS, '
+				'If true, then score ratios are rounded to two decimal places before passing back to the LMS, '
 					. 'e.g. "5 out of 6" is sent to the LMS as "0.83". (This is the historical behavior of WeBWorK.) '
 					. 'If false, then no rounding is performed.'
 			),
